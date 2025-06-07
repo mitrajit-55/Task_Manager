c6cab1a8a0bb992fe3fc7f8db87d485f888f1f89
mitrajit-55 <163036295+mitrajit-55@users.noreply.github.com>
2025-06-07 21:45:24 +0530

    updated responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-indigo-800 text-white py-3'>
-        <div className="logo">
-            <span class="font-bold text-xl mx-9 my-2">TaskFlow</span>
-        </div>
-        <ul className="flex gap-8 mx-9">
-            <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-            <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
-        </ul>
-        </nav>
+    <nav className='flex flex-col md:flex-row md:justify-between items-center bg-indigo-800 text-white py-3 px-6'>
+      <div className="logo mb-2 md:mb-0">
+        <span className="font-bold text-xl">TaskFlow</span>
+      </div>
+      <ul className="flex flex-col md:flex-row gap-4 md:gap-8 items-center">
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
+      </ul>
+    </nav>
   )
 }
 
